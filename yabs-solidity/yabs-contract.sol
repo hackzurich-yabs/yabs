88779cb7352b226c0e8a1599591580e6f81c397c
Karol Kowalski <ordon1313@gmail.com>
2017-09-15 23:56:35 +0200

    [Solidity] Create create sell offer

--- yabs-solidity/yabs-contract.sol
+++ yabs-solidity/yabs-contract.sol
@@ -1,33 +1,42 @@
 pragma solidity ^0.4.16;
 
+
 contract Yabs {
 
-  mapping (address => mapping (address => uint256)) public balances;
-  mapping (address => mapping (uint256 => Offer)) public buyOffers;
+    mapping (address => mapping (address => uint256)) public balances;
 
-  struct Offer {
+    mapping (address => mapping (uint256 => Offer)) public buyOffers;
+
+    mapping (address => mapping (uint256 => Offer)) public sellOffers;
+
+    struct Offer
+    {
     address retailer;
     uint256 points;
     uint256 yabs;
-  }
+    }
 
-  function Yabs() {
-  }
+    function Yabs() {
+    }
 
-  function getBalance(address user, address retailer) constant returns (uint256) {
-    return balances[user][retailer];
-  }
+    function getBalance(address user, address retailer) constant returns (uint256) {
+        return balances[user][retailer];
+    }
 
-  function addPoints(address user, uint256 points) {
-    balances[user][msg.sender] = balances[user][msg.sender] + points;
-  }
+    function addPoints(address user, uint256 points) {
+        balances[user][msg.sender] = balances[user][msg.sender] + points;
+    }
 
-  function claimPromoCode(address retailer, uint256 points) {
-    assert(balances[msg.sender][retailer] >= points);
-    balances[msg.sender][retailer] = balances[msg.sender][retailer] - points;
-  }
+    function claimPromoCode(address retailer, uint256 points) {
+        assert(balances[msg.sender][retailer] >= points);
+        balances[msg.sender][retailer] = balances[msg.sender][retailer] - points;
+    }
 
-  function createBuyOffer(uint256 id, address retailer, uint256 points, uint256 yabs) {
-    buyOffers[msg.sender][id] = Offer(retailer, points, yabs);
-  }
+    function createBuyOffer(uint256 id, address retailer, uint256 points, uint256 yabs) {
+        buyOffers[msg.sender][id] = Offer(retailer, points, yabs);
+    }
+
+    function createSellOffer(uint256 id, address retailer, uint256 points, uint256 yabs) {
+        sellOffers[msg.sender][id] = Offer(retailer, points, yabs);
+    }
 }
