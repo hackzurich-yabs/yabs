58cbd6185add4b169bfdc234d08540224d972b9e
Karol Kowalski <ordon1313@gmail.com>
2017-09-15 23:55:20 +0200

    [Solidity] Create create buy offer

--- yabs-solidity/yabs-contract.sol
+++ yabs-solidity/yabs-contract.sol
@@ -3,20 +3,31 @@ pragma solidity ^0.4.16;
 contract Yabs {
 
   mapping (address => mapping (address => uint256)) public balances;
+  mapping (address => mapping (uint256 => Offer)) public buyOffers;
+
+  struct Offer {
+    address retailer;
+    uint256 points;
+    uint256 yabs;
+  }
 
   function Yabs() {
   }
 
-  function getBalance(address user, address retailer) constant returns (uint) {
+  function getBalance(address user, address retailer) constant returns (uint256) {
     return balances[user][retailer];
   }
 
-  function addPoints(address user, uint points) {
+  function addPoints(address user, uint256 points) {
     balances[user][msg.sender] = balances[user][msg.sender] + points;
   }
 
-  function claimPromoCode(address retailer, uint points) {
+  function claimPromoCode(address retailer, uint256 points) {
     assert(balances[msg.sender][retailer] >= points);
     balances[msg.sender][retailer] = balances[msg.sender][retailer] - points;
+  }
+
+  function createBuyOffer(uint256 id, address retailer, uint256 points, uint256 yabs) {
+    buyOffers[msg.sender][id] = Offer(retailer, points, yabs);
   }
 }
