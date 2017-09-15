6b5bd546347f57cdec64c4ef249313745ccadb2e
Karol Kowalski <ordon1313@gmail.com>
2017-09-15 23:21:55 +0200

    [Solidity] Create getBalance

--- yabs-solidity/yabs-contract.sol
+++ yabs-solidity/yabs-contract.sol
@@ -2,6 +2,12 @@ pragma solidity ^0.4.16;
 
 contract Yabs {
 
+  mapping (address => mapping (address => uint256)) public balances;
+
   function Yabs() {
+  }
+
+  function getBalance(address user, address retailer) constant returns (uint) {
+    return balances[user][retailer];
   }
 }
