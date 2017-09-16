08778c86a7ba68a08a0b84e63e0c249da1d369ae
ghostbuster <kghost0@gmail.com>
2017-09-16 13:21:13 +0200

    Add loader to buySell offers

--- yabs-android/app/src/main/java/io/github/yabs/yabsmobile/BuySellOfferActivity.kt
+++ yabs-android/app/src/main/java/io/github/yabs/yabsmobile/BuySellOfferActivity.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.elpassion.android.commons.recycler.adapters.basicAdapterWithLayoutAndBinder
 import com.elpassion.android.commons.recycler.basic.ViewHolderBinder
 import com.elpassion.android.view.hide
 import com.elpassion.android.view.show
 import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.IoScheduler
@@ -35,6 +37,7 @@ abstract class BuySellOfferActivity : AppCompatActivity() {
         setContentView(R.layout.offers_list)
         offersList.layoutManager = LinearLayoutManager(this)
         disposable = api.map { extractOffers(it) }
+                .bindLoader(progressBar)
                 .subscribe({
                     offersList.adapter = basicAdapterWithLayoutAndBinder(it, R.layout.offer_field, this::bindOffer)
                 }, {
@@ -52,6 +55,7 @@ abstract class BuySellOfferActivity : AppCompatActivity() {
             disposable = createOffer(offerData = OfferData(userKey = walletManager.getWallet().address, uid = 0L, points = points, yabsPoints = yabs, retailersKey = retailer.publicKey))
                     .subscribeOn(IoScheduler())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .bindLoader(progressBar)
                     .subscribe({
                         Toast.makeText(this, "Udalo sie!!", Toast.LENGTH_LONG).show()
                     }, {
@@ -68,6 +72,7 @@ abstract class BuySellOfferActivity : AppCompatActivity() {
             disposable = fulFillOffer(offer.uid, walletManager.getWallet().address)
                     .subscribeOn(IoScheduler())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .bindLoader(progressBar)
                     .subscribe({
                         Toast.makeText(context, "Udalo sie!!", Toast.LENGTH_LONG).show()
                     }, {
@@ -97,4 +102,16 @@ abstract class BuySellOfferActivity : AppCompatActivity() {
     }
 
     abstract fun createOffer(offerData: OfferData): Completable
+}
+
+private fun Completable.bindLoader(progressBar: View): Completable {
+    return doOnSubscribe { progressBar.show() }
+            .doOnDispose { progressBar.hide() }
+            .doOnComplete { progressBar.hide() }
+}
+
+fun <T> Observable<T>.bindLoader(progressBar: View): Observable<T> {
+    return doOnSubscribe { progressBar.show() }
+            .doOnDispose { progressBar.hide() }
+            .doOnComplete { progressBar.hide() }
 }
