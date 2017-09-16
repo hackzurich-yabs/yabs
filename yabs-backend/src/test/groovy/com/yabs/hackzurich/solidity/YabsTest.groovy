50c77d17bd9a60a34ab965c534153ea209db486a
Karol Kowalski <ordon1313@gmail.com>
2017-09-16 07:38:23 +0200

    shouldGiveRetailersCoinsToMobile

--- yabs-backend/src/test/groovy/com/yabs/hackzurich/solidity/YabsTest.groovy
+++ yabs-backend/src/test/groovy/com/yabs/hackzurich/solidity/YabsTest.groovy
@@ -3,10 +3,13 @@ package com.yabs.hackzurich.solidity
 import groovy.transform.CompileStatic
 import org.junit.Ignore
 import org.junit.Test
+import org.web3j.abi.datatypes.Address
+import org.web3j.abi.datatypes.generated.Uint256
 import org.web3j.crypto.Credentials
 import org.web3j.crypto.WalletUtils
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.JsonRpc2_0Web3j
+import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.protocol.infura.InfuraHttpService
 import org.web3j.tx.Contract
 import org.web3j.tx.Transfer
@@ -17,9 +20,13 @@ import static org.web3j.tx.ManagedTransaction.GAS_PRICE
 @CompileStatic
 class YabsTest {
 
-    private File credentialsFile = new File(ClassLoader.getSystemClassLoader().getResource("rinkeby.json").file)
-    private Credentials credentials = WalletUtils.loadCredentials("rinkeby", credentialsFile)
-    private Web3j web3j = new JsonRpc2_0Web3j(new InfuraHttpService("https://rinkeby.infura.io/0ZevQ4HkUCzCVBOsYZcQ"))
+    private final File configuration = new File("src/main/resources/solidityConfiguration.properties")
+    private final String contractAddress = configuration.readLines().first().split("=")[1]
+    private final File credentialsFile = new File(ClassLoader.getSystemClassLoader().getResource("rinkeby.json").file)
+    private final Credentials credentials = WalletUtils.loadCredentials("rinkeby", credentialsFile)
+    private final String url = "https://rinkeby.infura.io/0ZevQ4HkUCzCVBOsYZcQ"
+    private final Web3j web3j = new JsonRpc2_0Web3j(new InfuraHttpService(url))
+    private final String mobileAddress = "0x216d80a85bd731ed66f3026e26560cc0316c4fa4"
 
     @Ignore
     @Test
@@ -27,7 +34,6 @@ class YabsTest {
         String contractAddress = YabsContract.deploy(web3j, credentials, GAS_PRICE, Contract.GAS_LIMIT, BigInteger.ZERO)
                 .get()
                 .contractAddress
-        File configuration = new File("src/main/resources/solidityConfiguration.properties")
         configuration.write("yabsAddress=$contractAddress")
         println(contractAddress)
     }
@@ -40,7 +46,7 @@ class YabsTest {
 
     @Ignore
     @Test
-    void giveEtherToRetailers() throws Exception {
+    void shouldGiveEtherToRetailers() throws Exception {
         giveOneEtherToAddress(addersOfRetailer("Coop"))
         giveOneEtherToAddress(addersOfRetailer("Fashwell"))
         giveOneEtherToAddress(addersOfRetailer("Siroop"))
@@ -50,7 +56,19 @@ class YabsTest {
     @Ignore
     @Test
     void shouldGiveSomeMoneyToMobile() throws Exception {
-        giveOneEtherToAddress("0xa8c9924051886c083a3149196605df2358988c47")
+        giveOneEtherToAddress(mobileAddress)
+    }
+
+    @Ignore
+    @Test
+    void shouldGiveRetailersCoinsToMobile() throws Exception {
+        giveRetailersCoinsToMobile("Coop", 1000L)
+        giveRetailersCoinsToMobile("Zalando", 99L)
+        giveRetailersCoinsToMobile("Fashwell", 200L)
+    }
+
+    private void giveRetailersCoinsToMobile(String retailerName, long points) {
+        loadContract(retailerCredentials(retailerName)).addPoints(new Address(mobileAddress), new Uint256(BigInteger.valueOf(points))).get()
     }
 
     private void giveOneEtherToAddress(String address) {
@@ -58,9 +76,16 @@ class YabsTest {
     }
 
     private String addersOfRetailer(String retailerName) {
+        return retailerCredentials(retailerName).address
+    }
+
+    private Credentials retailerCredentials(String retailerName) {
         String name = retailerName
         def file = new File("src/main/resources/retailers/${name}.json")
-        def credentials = WalletUtils.loadCredentials("password", file)
-        return credentials.address
+        return WalletUtils.loadCredentials("password", file)
+    }
+
+    private YabsContract loadContract(Credentials credentials) {
+        return YabsContract.load(contractAddress, web3j, credentials, GAS_PRICE, Contract.GAS_LIMIT)
     }
 }
