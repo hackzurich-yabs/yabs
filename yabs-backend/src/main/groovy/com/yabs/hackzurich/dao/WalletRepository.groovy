0f0d57942bcfa97fa0a8d1acd2e0527d15b90459
Karol Kowalski <ordon1313@gmail.com>
2017-09-16 04:06:44 +0200

    Create read only wallet

--- yabs-backend/src/main/groovy/com/yabs/hackzurich/dao/WalletRepository.groovy
+++ yabs-backend/src/main/groovy/com/yabs/hackzurich/dao/WalletRepository.groovy
@@ -9,6 +9,7 @@ class WalletRepository {
 
     private static String password = "password"
     private static File directory = Files.createTempDirectory("wallets").toFile()
+    private static String readOnlyCredentials = WalletUtils.generateLightNewWalletFile(password, directory)
 
 
     static String crateNewWalletFile() {
@@ -17,5 +18,9 @@ class WalletRepository {
 
     static Credentials readCredentials(String walletFile) {
         return WalletUtils.loadCredentials(password, new File(directory, walletFile))
+    }
+
+    static Credentials readOnlyCredentials() {
+        return WalletUtils.loadCredentials(password, new File(directory, readOnlyCredentials))
     }
 }
