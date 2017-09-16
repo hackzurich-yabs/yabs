49c73e23b7feba2ce542c64e520beeba87c30adf
Szop-Kradziej <karolina.drabarz@gmail.com>
2017-09-16 02:58:00 +0200

    [Backend] Fix getBalancesForUser

--- yabs-backend/src/main/groovy/com/yabs/hackzurich/service/BalancesService.groovy
+++ yabs-backend/src/main/groovy/com/yabs/hackzurich/service/BalancesService.groovy
@@ -1,43 +1,35 @@
-package com.yabs.hackzurich.service
+package com.yabs.hackzurich.service;
 
-import com.yabs.hackzurich.dto.BalanceData
-import com.yabs.hackzurich.solidity.SolidityUtils
-import com.yabs.hackzurich.solidity.Yabs
-import groovy.transform.CompileStatic
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
-import org.web3j.abi.datatypes.Address
-import org.web3j.crypto.Credentials
-import org.web3j.crypto.WalletUtils
-import org.web3j.protocol.Web3j
-import org.web3j.protocol.core.JsonRpc2_0Web3j
-import org.web3j.protocol.infura.InfuraHttpService
-import org.web3j.utils.Convert
+import com.yabs.hackzurich.dto.BalanceData;
+import com.yabs.hackzurich.solidity.SolidityUtils;
+import com.yabs.hackzurich.solidity.YabsContract;
+import groovy.transform.CompileStatic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.web3j.abi.datatypes.Address;
 
 @Service
 @CompileStatic
 class BalancesService {
 
+    private final RetailersService retailersService;
+
     @Autowired
-    private RetailersService retailersService
-
-    private File credentialsFile = new File(ClassLoader.getSystemClassLoader().getResource("rinkeby.json").file)
-    private Credentials credentials = WalletUtils.loadCredentials("rinkeby", credentialsFile)
-    private Web3j web3j = new JsonRpc2_0Web3j(new InfuraHttpService("https://rinkeby.infura.io/0ZevQ4HkUCzCVBOsYZcQ"))
-
-    List<BalanceData> getBalancesForUser(String userPublicKey) {
-
-        Yabs yabsContract = getYabsContract()
-
-        return retailersService.list().stream()
-                .map{retailer -> yabsContract.getBalance(new Address(userPublicKey), new Address(retailer.name))}
-                .collect()
+    BalancesService(RetailersService retailersService) {
+        this.retailersService = retailersService;
     }
 
-    private Yabs getYabsContract() {
-        return Yabs.load(SolidityUtils.yabsAddress,
-                web3j,
-                credentials,
-                Convert.toWei("21", Convert.Unit.GWEI).toBigInteger(), BigInteger.valueOf(338742L))
+    List<BalanceData> getBalancesForUser(final String userPublicKey) {
+
+        final YabsContract contract = SolidityUtils.getYabsContract();
+
+        retailersService.list()
+                .collect {
+            new BalanceData(
+                    it.publicKey,
+                    it.name,
+                    new BigInteger(contract.getBalance(new Address(userPublicKey), new Address(it.publicKey)).get().toString())
+            )
+        }
     }
 }
--- yabs-backend/src/main/groovy/com/yabs/hackzurich/solidity/SolidityUtils.groovy
+++ yabs-backend/src/main/groovy/com/yabs/hackzurich/solidity/SolidityUtils.groovy
@@ -1,6 +1,24 @@
-package com.yabs.hackzurich.solidity;
+package com.yabs.hackzurich.solidity
+
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.WalletUtils
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.JsonRpc2_0Web3j
+import org.web3j.protocol.infura.InfuraHttpService
+import org.web3j.utils.Convert;
 
 class SolidityUtils {
 
-    static final String yabsAddress = "0x358552e3c1a6e1071426d841b4ed301a56f30612"
+    private static final String yabsAddress = "0x358552e3c1a6e1071426d841b4ed301a56f30612"
+
+    private static final File credentialsFile = new File(ClassLoader.getSystemClassLoader().getResource("rinkeby.json").file)
+    private static final Credentials credentials = WalletUtils.loadCredentials("rinkeby", credentialsFile)
+    private static final Web3j web3j = new JsonRpc2_0Web3j(new InfuraHttpService("https://rinkeby.infura.io/0ZevQ4HkUCzCVBOsYZcQ"))
+
+    static final YabsContract getYabsContract() {
+        return YabsContract.load(SolidityUtils.yabsAddress,
+                web3j,
+                credentials,
+                Convert.toWei("21", Convert.Unit.GWEI).toBigInteger(), BigInteger.valueOf(338742L))
+    }
 }
