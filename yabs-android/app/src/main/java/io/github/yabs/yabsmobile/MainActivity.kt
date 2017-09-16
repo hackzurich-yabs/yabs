0ace331c915a5a60316dfbeb2a31346d0e5891d4
ghostbuster <kghost0@gmail.com>
2017-09-16 02:20:52 +0200

    Unsubscribe from api on destroyĻĻ

--- yabs-android/app/src/main/java/io/github/yabs/yabsmobile/MainActivity.kt
+++ yabs-android/app/src/main/java/io/github/yabs/yabsmobile/MainActivity.kt
@@ -1,12 +1,12 @@
 package io.github.yabs.yabsmobile
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.elpassion.android.commons.recycler.adapters.basicAdapterWithLayoutAndBinder
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.IoScheduler
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.retailer_item.view.*
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private var walletFileName by sharedPrefsProvider<String>().asProperty("walletFile")
     private val retailersApi by lazy { BalancesApi.INSTANCE }
+    private var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         val wallet = WalletUtils.loadCredentials("password", File(credentialsVault, walletFileName))
 
         retailerListView.layoutManager = LinearLayoutManager(this)
-        retailersApi.balances(wallet.ecKeyPair.publicKey.toString())
+        disposable = retailersApi.balances(wallet.ecKeyPair.publicKey.toString())
                 .subscribeOn(IoScheduler())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
@@ -48,22 +49,10 @@ class MainActivity : AppCompatActivity() {
                 }, {
                     Log.e("kasper", "$it")
                 })
-//            val integrator = IntentIntegrator(this)
-//            integrator.initiateScan()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == IntentIntegrator.REQUEST_CODE) {
-            val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-            if (scanResult != null) {
-                // handle scan result
-                Log.e("kasper", "toString() returns: " + scanResult.toString());
-            } else {
-                // else continue with any other code you need in the method
-                Log.e("kasper", "scanResult is null.")
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+    override fun onDestroy() {
+        disposable?.dispose()
+        super.onDestroy()
     }
 }
