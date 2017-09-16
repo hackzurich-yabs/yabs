7a561736b37d053d9322b1f7d751674c1ba54e42
ghostbuster <kghost0@gmail.com>
2017-09-16 08:28:07 +0200

    Remove scalar retrofit adapter

--- yabs-android/app/src/main/java/io/github/yabs/yabsmobile/Retrofit.kt
+++ yabs-android/app/src/main/java/io/github/yabs/yabsmobile/Retrofit.kt
@@ -6,7 +6,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-//import retrofit2.converter.scalars.ScalarsConverterFactory
 
 val gsonProvider by lazy { Gson() }
 
@@ -14,7 +13,6 @@ val retrofit by lazy {
     Retrofit.Builder()
             .baseUrl(contextProvider().getString(R.string.base_url))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gsonProvider))
             .client(OkHttpClient().newBuilder().apply {
                 addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
