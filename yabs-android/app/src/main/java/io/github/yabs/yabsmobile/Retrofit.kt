8e65e9c3bb3bda5fbc663cd983ffa40927b98384
Karol Kowalski <ordon1313@gmail.com>
2017-09-16 06:20:51 +0200

    Comment converter

--- yabs-android/app/src/main/java/io/github/yabs/yabsmobile/Retrofit.kt
+++ yabs-android/app/src/main/java/io/github/yabs/yabsmobile/Retrofit.kt
@@ -6,7 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
+//import retrofit2.converter.scalars.ScalarsConverterFactory
 
 val gsonProvider by lazy { Gson() }
 
@@ -14,7 +14,7 @@ val retrofit by lazy {
     Retrofit.Builder()
             .baseUrl(contextProvider().getString(R.string.base_url))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(ScalarsConverterFactory.create())
+//            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gsonProvider))
             .client(OkHttpClient().newBuilder().apply {
                 addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
